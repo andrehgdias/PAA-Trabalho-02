c319c31b7c216eee72d8b296103dcf15e973d7fb
André <andredias.us@gmail.com>
2018-11-28 20:03:25 -0200

    Adicionando projeto em branco

---
+++ src/trabalhopaa02/TrabalhoPAA02.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalhopaa02;
+
+/**
+ *
+ * @author andre
+ */
+public class TrabalhoPAA02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
