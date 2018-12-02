b754195a45e28c20eecb1944043d0dd0d9790778
André <andredias.us@gmail.com>
2018-12-01 22:18:44 -0200

    Limpando o código, ainda falta melhorar, versão aparentemente estável, precisa de testes.

--- src/model/AssociacaoTarefas.java
+++ src/model/AssociacaoTarefas.java
@@ -11,22 +11,6 @@ package model;
  */
 public class AssociacaoTarefas {
 
-    public static boolean validar(int vetor[], int pessoasDesignadas, int solucaoAtual, int melhorSolucao) {
-        int i, candidato = vetor[pessoasDesignadas - 1];
-
-        if (solucaoAtual > melhorSolucao) {
-            return false;
-        }
-
-        for (i = 0; i < pessoasDesignadas - 1; i++) {
-            if (vetor[i] == candidato) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
     public static boolean checarConflito(int linhaCandidato, int valor, NoSolucoes objSolucao) {
         if (objSolucao.getSomaSolucaoAtual() > objSolucao.getMelhorSolucao()) {
             objSolucao.setSomaSolucaoAtual(objSolucao.getSomaSolucaoAtual() - valor);
@@ -47,11 +31,14 @@ public class AssociacaoTarefas {
         if (colunaInicio == colunas) {
             return true; // encontrou uma solução
         }
-       
+        int pularLinha = 0;
+        if (!primeirasolucao) {
+            pularLinha = linhaInicio;
+        }
 
         boolean sol;
 
-        for (int i = 0; i < linhas; i++) {
+        for (int i = pularLinha; i < linhas; i++) {
             objSolucao.setSomaSolucaoAtual(objSolucao.getSomaSolucaoAtual() + matriz[i][colunaInicio]);
             if (checarConflito(i, matriz[i][colunaInicio], objSolucao)) { //  //true = sem conflitos, continua
 
@@ -82,42 +69,5 @@ public class AssociacaoTarefas {
 
         return false;
 
-//        int i, j = 0;
-//        int vetor[] = new int[colunas];
-//        int vetorSolucao[];
-//        vetorSolucao = new int[colunas];
-//        int melhorSolucao = Integer.MAX_VALUE, solucaoAtual = 0;
-//        
-//        boolean continua = true;
-////        boolean continua_j = true;
-//        
-//        for(i = 0; i < linhas && continua; i++){
-////                continua_j = true;
-//            for( ; j < colunas; j++){ // && continua_j
-//                
-//                System.out.println(matriz[i][j]); //para mostrar o caminho
-//                vetor[j] = matriz[i][j];
-//                vetorSolucao[j] = i + 1;
-//                solucaoAtual += matriz[i][j];
-//                if(!validar(vetor, i + 1, solucaoAtual, melhorSolucao)){
-//                   solucaoAtual -= matriz[i][j];
-////                    continua_j = false;
-//                    break;
-//                }
-//                else i = 0;
-//            }
-//
-//            if(j == colunas){ //encheu o vetor de solução
-//                melhorSolucao = solucaoAtual;
-//                continua = false;
-//                i++; //atualizando para a próxima iteração (começando uma nova solução)
-//            }
-//
-//        }
-//        
-//        for(i = 0; i < colunas; i++){
-//            System.out.println("\n" + vetorSolucao[i]);
-//        }
     }
-
 }
--- src/trabalhopaa02/Principal.java
+++ src/trabalhopaa02/Principal.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package trabalhopaa02;
 
 import model.AssociacaoTarefas;
@@ -30,32 +25,9 @@ public class Principal {
                           { 14,15,13,22 },
                           { 11,17,19,23 },
                           {17,14,20,28}};
-//        
-//        matriz[0][0] = 5;
-//        matriz[0][1] = 5;
-//        matriz[0][2] = 6;
-//        matriz[0][3] = 9;
-//        matriz[1][0] = 3;
-//        matriz[1][1] = 1;
-//        matriz[1][2] = 4;
-//        matriz[1][3] = 4;
-//        matriz[2][0] = 3;
-//        matriz[2][1] = 5;
-//        matriz[2][2] = 13;
-//        matriz[2][3] = 3;
-//        matriz[3][0] = 3;
-//        matriz[3][1] = 9;
-//        matriz[3][2] = 14;
-//        matriz[3][3] = 10;
 
         NoSolucoes solucao = new NoSolucoes();
         
-//        int solucao[] = {-1,-1,-1,-1};
-//        int solucaoTemporaria[] = {-1,-1,-1,-1};
-//        
-//        int melhorSolucao = Integer.MAX_VALUE, solucaoAtual = 0;
-
-            
             for(int k=0; k < 4; k++) solucao.getVetorSolucaoTemporaria()[k] = -1;
             AssociacaoTarefas.resolverAssociacaoDeTarefas(matriz, 4, 4, 0, 0, solucao, true);//solucao.getVetorSolucao(), olucao.getVetorSolucaoTemporaria(), resposta, solucao.getMelhorSolucao(), solucao.getSomaSolucaoAtual());
             
