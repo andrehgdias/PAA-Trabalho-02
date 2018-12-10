08ef54d1e248586267a325670265558d5d1f6519
thalesvini <thales.britoue@gmail.com>
2018-12-10 16:08:41 -0200

    Implementação do Merge Sort

--- src/model/MergeSort.java
+++ src/model/MergeSort.java
@@ -5,10 +5,77 @@
  */
 package model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author thale
  */
 public class MergeSort {
+    
+    private ArrayList<Item> inputArray;
+     
+    public ArrayList<Item> getSortedArray() {
+        return inputArray;
+    }
+ 
+    public MergeSort(ArrayList<Item> inputArray){
+        this.inputArray = inputArray;
+    }
+     
+    public void sortGivenArray(){       
+        divide(0, this.inputArray.size()-1);
+    }
+     
+    public void divide(int startIndex,int endIndex){
+         
+        //Divide till you breakdown your list to single element
+        if(startIndex<endIndex && (endIndex-startIndex)>=1){
+            int mid = (endIndex + startIndex)/2;
+            divide(startIndex, mid);
+            divide(mid+1, endIndex);        
+             
+            //merging Sorted array produce above into one sorted array
+            merger(startIndex,mid,endIndex);            
+        }       
+    }   
+     
+    public void merger(int startIndex,int midIndex,int endIndex){
+         
+        //Below is the mergedarray that will be sorted array Array[i-midIndex] , Array[(midIndex+1)-endIndex]
+        ArrayList<Item> mergedSortedArray = new ArrayList<Item>();
+         
+        int leftIndex = startIndex;
+        int rightIndex = midIndex+1;
+         
+        while(leftIndex<=midIndex && rightIndex<=endIndex){
+            if(inputArray.get(leftIndex).getValor()<=inputArray.get(rightIndex).getValor()){
+                mergedSortedArray.add(inputArray.get(leftIndex));
+                leftIndex++;
+            }else{
+                mergedSortedArray.add(inputArray.get(rightIndex));
+                rightIndex++;
+            }
+        }       
+         
+        //Either of below while loop will execute
+        while(leftIndex<=midIndex){
+            mergedSortedArray.add(inputArray.get(leftIndex));
+            leftIndex++;
+        }
+         
+        while(rightIndex<=endIndex){
+            mergedSortedArray.add(inputArray.get(rightIndex));
+            rightIndex++;
+        }
+         
+        int i = 0;
+        int j = startIndex;
+        //Setting sorted array to original one
+        while(i<mergedSortedArray.size()){
+            inputArray.set(j, mergedSortedArray.get(i++));
+            j++;
+        }
+    }
     
 }
